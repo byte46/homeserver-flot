bdca31954828d78b37ca14a00deec4242b2bf5cc
Serge AKA Byte <byte.kgd@gmail.com>
2016-09-16 22:13:39 +0300

    Вызов rrdfetch из питона, скрипт в отдельном файле

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
+# -*- coding: utf-8 -*
 import json
+import subprocess
+
 data = []
-with open('temperature.txt', 'r') as f:
-    for line in f:
-        if line.strip() and ':' not in line:
-            z = [x for x in line.rstrip().split(' ') if x]
-            for i in z:
-                data.append({'label': i, 'data':[]})
-        elif line.strip() and ':' in line:
-            d = []
-            time, rrd_data = line.rstrip().split(':')
-            d=[x for x in rrd_data.rstrip().split(' ') if x]
-            for k, i in enumerate(d):
-                data[k]['data'].append([int(time)*1000,i])
-            #print d
+labels = {
+        "out_temp":("Улица", "#2863E8"),
+        "room_temp": ("Комната", "#0A4C11"),
+        "kitchen_temp": ("Кухня", "#E51226"),
+        "bathroom_temp": ("Ванная", "#FF21AD"),
+        "radiator_temp": ("Батарея", "#FF0000"),
+        "balcony_temp_temp": ("Балкон", "#3F0290")
+        }
+
+poutput = subprocess.Popen('export LC_NUMERIC="C.UTF-8";rrdtool fetch temperature.rrd -s-24h AVERAGE', shell=True, stdout=subprocess.PIPE)
+for line in poutput.stdout:
+    if line.strip() and ':' not in line:
+        z = [x for x in line.rstrip().split(' ') if x]
+        for i in z:
+            data.append({'label': labels[i][0], "color": labels[i][1], 'data':[]})
+    elif line.strip() and ':' in line:
+        d = []
+        time, rrd_data = line.rstrip().split(':')
+        d=[x for x in rrd_data.rstrip().split(' ') if x]
+        for k, i in enumerate(d):
+            data[k]['data'].append([int(time)*1000,i])
+        #print d
 print json.dumps(data)
---
+++ temp.js
@@ -0,0 +1,92 @@
+$(function() {
+
+    String.prototype.lpad = function(padString, length) {
+        var str = this;
+        while (str.length < length)
+            str = padString + str;
+        return str;
+    }
+
+    $("<div id='tooltip'></div>").css({
+		position: "absolute",
+		display: "none",
+		border: "1px solid #fdd",
+		padding: "2px",
+		"background-color": "#fee",
+		opacity: 0.80
+	}).appendTo("body");
+
+    $("#placeholder").bind("plothover", function (event, pos, item) {
+
+		var str = "(" + pos.x.toFixed(2) + ", " + pos.y.toFixed(2) + ")";
+		$("#hoverdata").text(str);
+
+		if (item) {
+			var x = new Date(item.datapoint[0]),
+				y = item.datapoint[1].toFixed(2);
+
+            hrs = x.getHours().toString();
+            mins = x.getMinutes().toString();
+            ttime = hrs.lpad("0",2) + ":" + mins.lpad("0", 2);
+			$("#tooltip").html(item.series.label + "," + ttime + " = " + y +"°C")
+				.css({top: item.pageY+5, left: item.pageX+5})
+				.fadeIn(200);
+		} else {
+			$("#tooltip").hide();
+		}
+});
+
+	var options = {
+		lines: {
+			show: true
+		},
+		points: {
+			show: false
+		},
+        grid: {
+            hoverable: true,
+            backgroundColor: "#FFE5B3"
+        },
+		xaxis: {
+            mode: "time",
+            timezone: "browser",
+            ticks: 40,
+            minTickSize: [1, "hour"]
+		},
+        yaxis: {
+            label: "Градусы цельсия"
+        },
+        zoom: {
+            interactive: true
+        },
+        pan: {
+            interactive: true
+        },
+        series: {
+            points: {
+                radius: 2
+            }
+        },
+        legend:
+        {
+            show: true,
+            position: "se",
+            container: "#legend",
+            noColumns: 3
+        }
+
+	};
+
+	function onDataReceived(series) {
+		//data = [ series ];
+        data = series;
+		$.plot("#placeholder", data, options);
+	}
+
+	$.ajax({
+		url: "/temp.json",
+		type: "GET",
+		dataType: "json",
+		success: onDataReceived
+	});
+});
